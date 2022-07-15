9dd2db0782b3e2cdbf15776af0f30b849c719598
rkroll17 <rkroll17@byui.edu>
2022-07-14 23:31:32 -0600

    admin is ready

--- admin.py
+++ admin.py
@@ -1,6 +1,6 @@
 import reddit_skimmer as rs
 #import DATABASE
-# import bot
+import bot
 import sched, time
 
 reddit_limiter = 0
@@ -8,12 +8,14 @@ reddit_limiter = 0
 s = sched.scheduler(time.time, time.sleep)
 def do_something(sc, reddit_limiter): 
     print("Doing stuff...")
-    # do your stuff
-    if reddit_limiter < 60:
-        reddit_limiter += 1
-    else:
-        reddit_limiter = 0
-    print(reddit_limiter)
+    
+    
+    bot.read_posts(0)
+    s.enter(120, 1, bot.post_daily_leaderboard, ())
+    s.enter(180, 1, bot.post_weekly_leaderboard, ())
+    s.enter(240, 1, bot.post_monthly_leaderboard, ())
+    s.enter(270, 1 , bot.post_yearly_leaderboard, ())
+    s.enter(180, 1, bot.post_alltime_leaderboard, ())
     #first variable is how long in seconds the scheduler runs
     sc.enter(60, 1, do_something, (sc, reddit_limiter,))
 
@@ -21,5 +23,10 @@ def do_something(sc, reddit_limiter):
 s.enter(60, 1, do_something, (s, reddit_limiter, ))
 s.run()
 
+# s1 = sched.scheduler(time.time, time.sleep)
+
+# #first variable is how long in seconds the scheduler runs
+# s1.enter(60, 1, bot.post_alltime_leaderboard, ( ))
+# s1.run()
 #code sourced from 
 # https://stackoverflow.com/questions/474528/what-is-the-best-way-to-repeatedly-execute-a-function-every-x-seconds
